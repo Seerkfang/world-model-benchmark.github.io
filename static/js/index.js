99e0effc867f1ef67d9f2d225d3d7548d8b2921c
Fang Yunhao <fangyunhao@Fangs-MacBook-Pro.local>
2024-12-15 16:03:10 -0800

    update.

--- static/js/index.js
+++ static/js/index.js
@@ -32,7 +32,9 @@ function loadTableData() {
       const tbody = document.querySelector("#mmmu-table tbody");
 
       // Prepare data for styling
+      console.log("start processing...")
       const proScores = prepareScoresForStyling(data.leaderboardData, "pro");
+      console.log("finish processing pro: ", proScores)
       const valScores = prepareScoresForStyling(
         data.leaderboardData,
         "validation"
