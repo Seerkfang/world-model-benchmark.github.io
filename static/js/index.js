f1af3296d59a6fa436c6cf1d5300dd026d8cff6d
Fang Yunhao <fangyunhao@Fangs-MBP.attlocal.net>
2025-01-19 23:53:57 -0800

    Update.

--- static/js/index.js
+++ static/js/index.js
@@ -81,14 +81,18 @@ function loadTableData() {
               <td>${nameCell}</td>
               <td>${row.info.size}</td>
               <td>${row.info.date}</td>
-              <td class="test-overall">${testOverall}</td>
-              <td class="hidden test-details">${applyStyle(
-                safeGet(row, "test.frameWise"),
-                testScores.frameWise[index]
+              <td class="pro-overall">${proOverall}</td>
+              <td class="hidden pro-details">${applyStyle(
+                safeGet(row, "pro.level1"),
+                proScores.level1[index]
               )}</td>
-              <td class="hidden test-details">${applyStyle(
-                safeGet(row, "test.temporal"),
-                testScores.temporal[index]
+              <td class="hidden pro-details">${applyStyle(
+                safeGet(row, "pro.level2"),
+                proScores.level2[index]
+              )}</td>
+              <td class="hidden pro-details">${applyStyle(
+                safeGet(row, "pro.level3"),
+                proScores.level3[index]
               )}</td>
               <td class="val-overall">${valOverall}</td>
               <td class="hidden val-details">${applyStyle(
@@ -111,18 +115,14 @@ function loadTableData() {
                 safeGet(row, "validation.gravity"),
                 valScores.gravity[index]
               )}</td>
-              <td class="pro-overall">${proOverall}</td>
-              <td class="hidden pro-details">${applyStyle(
-                safeGet(row, "pro.level1"),
-                proScores.level1[index]
+              <td class="test-overall">${testOverall}</td>
+              <td class="hidden test-details">${applyStyle(
+                safeGet(row, "test.frameWise"),
+                testScores.frameWise[index]
               )}</td>
-              <td class="hidden pro-details">${applyStyle(
-                safeGet(row, "pro.level2"),
-                proScores.level2[index]
-              )}</td>
-              <td class="hidden pro-details">${applyStyle(
-                safeGet(row, "pro.level3"),
-                proScores.level3[index]
+              <td class="hidden test-details">${applyStyle(
+                safeGet(row, "test.temporal"),
+                testScores.temporal[index]
               )}</td>
             `;
         tbody.appendChild(tr);
